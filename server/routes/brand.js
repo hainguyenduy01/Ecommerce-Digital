0bd1911e9854462acc1723b7462af24578311599
Nguyen Duy Hai <nguyenduyhai308@gmail.com>
2023-07-26 23:02:16 +0700

    finish coupon

---
+++ server/routes/brand.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const ctrls = require('../controllers/brand');
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifToken');
+router.post('/', [verifyAccessToken, isAdmin], ctrls.createNewBrand);
+router.get('/', ctrls.getBrands);
+router.put('/:bid', [verifyAccessToken, isAdmin], ctrls.updateBrand);
+router.delete('/:bid', [verifyAccessToken, isAdmin], ctrls.deleteBrand);
+module.exports = router;
