137511d629ec6a7656128add7aabd157159087b3
Nguyen Duy Hai <nguyenduyhai308@gmail.com>
2023-07-26 13:05:07 +0700

    finish rating product

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -124,8 +124,19 @@ const ratings = asyncHandler(async (req, res) => {
 			{ new: true },
 		);
 	}
+	// Sum ratings
+	const updatedProduct = await Product.findById(pid);
+	const ratingCount = updatedProduct.ratings.length;
+	const sumRatings = updatedProduct.ratings.reduce(
+		(sum, el) => sum + +el.star,
+		0,
+	);
+	updatedProduct.totalRating = Math.round((sumRatings * 10) / ratingCount) / 10;
+	await updatedProduct.save();
+
 	return res.status(200).json({
 		status: true,
+		updatedProduct,
 	});
 });
 module.exports = {
