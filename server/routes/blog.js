691151de05175a1fb8193b5bc4affa2e2b9dee09
Nguyen Duy Hai <nguyenduyhai308@gmail.com>
2023-07-26 22:51:21 +0700

    finish CRUD blog

---
+++ server/routes/blog.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifToken');
+const ctrls = require('../controllers/blog');
+
+router.get('/', ctrls.getBlogs);
+router.post('/', [verifyAccessToken, isAdmin], ctrls.createNewBlog);
+router.get('/one/:bid', ctrls.getBlog);
+router.put('/like/:bid', [verifyAccessToken], ctrls.likeBlog);
+router.put('/dislike/:bid', [verifyAccessToken], ctrls.dislikeBlog);
+router.put('/:bid', [verifyAccessToken, isAdmin], ctrls.updateBlog);
+router.delete('/:bid', [verifyAccessToken, isAdmin], ctrls.deleteBlog);
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,14 @@ const userRouter = require('./user');
 const productRouter = require('./product');
 const productCategoryRouter = require('./productCategory');
 const blogCategoryRouter = require('./blogCategory');
+const blogRouter = require('./blog');
 const { notFound, errHandler } = require('../middlewares/errHandler');
 const initRoutes = (app) => {
 	app.use('/api/user', userRouter);
 	app.use('/api/product', productRouter);
 	app.use('/api/blogcategory', blogCategoryRouter);
 	app.use('/api/prodcategory', productCategoryRouter);
-
+	app.use('/api/blog', blogRouter);
 	app.use(notFound);
 	app.use(errHandler);
 };
